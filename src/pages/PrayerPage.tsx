31e1ca7ffaafa8ebb1d58d0f18eafe4c912d2e9d
Magdy Saber. <magdy.saber@outlook.com>
2026-03-08 20:17:38 +0000

    Preceding changes

--- src/pages/PrayerPage.tsx
+++ src/pages/PrayerPage.tsx
@@ -4,8 +4,7 @@ import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { useStreak } from "@/hooks/useStreak";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Progress } from "@/components/ui/progress";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { cn, getArabicDayName, getHijriDate, getGregorianDateArabic } from "@/lib/utils";
+import { cn, getArabicDayName, getArabicDayShort, getHijriDate, getGregorianDateArabic } from "@/lib/utils";
 import {
   calculatePrayerTimes,
   formatArabicTime,
